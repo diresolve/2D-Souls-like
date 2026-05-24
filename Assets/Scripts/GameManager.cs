60d962ad3cfde27613c4aa684a169c9749f4b512
leakl <leakl@BALROG>
2026-05-24 12:58:54 +0200

    fix: typo in GameManager, player stays still when blocking, closed boss area

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance != null && Instance != null)
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
             return;
